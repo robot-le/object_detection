edd7e3d694a143112522e9ea2752961aca08c0f5
Asan Dzhuzbaev <dzhuzbaevasan@gmail.com>
2024-05-14 19:07:21 +0600

    Add class for detected objects

--- detection/models.py
+++ detection/models.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class Image(models.Model):
     image = models.ImageField('Uploaded image')
+
+
+class DetectedObjects(models.Model):
+    label = models.CharField(max_length=255)
+    confidence = models.FloatField()
+    bounding_box = models.TextField()
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
--- detection/views.py
+++ detection/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import DetectedObjects
 
 from .serializers import ImageUploadSerializer
 from detection_module.detect_object import detect_objects
@@ -16,6 +17,15 @@ class ImageProcessAPIView(APIView):
         if serializer.is_valid():
             uploaded_file = serializer.validated_data.get('image')
             detected_objects = detect_objects(uploaded_file)
+            image_obj = serializer.save()
+            for obj in detected_objects:
+                detected_obj = DetectedObjects(
+                    label=obj.get('label', ''),
+                    confidence=obj.get('confidence', 0),
+                    bounding_box=', '.join([str(coord[1]) for coord in obj.get('box', {}).items()]),
+                    image=image_obj,
+                )
+                detected_obj.save()
             return Response(
                 {'detected_objects': detected_objects},
                 status=status.HTTP_201_CREATED
